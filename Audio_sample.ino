047c6d29646cffb42bbf686682d1d561de9fad47
stevstrong <stevestrong@online.de>
2015-10-08 13:39:08 +0200

    use ADC7...4 for multiplexed conversion
    - cleanup code;
    - don't store very first conversion result

--- Audio_sample.ino
+++ Audio_sample.ino
@@ -1,4 +1,4 @@
-// Audio signal from ADC0 continuously samped, alternatively stored in 2 different buffers,
+// Audio signal from ADC7...4 continuously samped, alternatively stored in 2 different buffers,
 // copied into the SD card cache, and then stored on SD card.
 // Based on RawWrite.ino example of SdFat library.
 /////////////////////////////////////////////////////////////////////////////////////
@@ -8,26 +8,22 @@
 
 #include <util/delay.h>
 
-#define DEBUG 1 // set to 1 to also send out info on the serial port
+#define DEBUG 1 // set to 1 to send out info on the serial port
 
-#define RXDATA 14
-#define RELAIS_PIN 8
-#define MAX_VAL 4 // number of consequtive increased value to detect transition
 #ifdef DEBUG
   #define ADC_OUT_PIN 9
   #define DBG_PIN 8
   static byte dbg = 0;
 #endif
-  static byte toggle = 0;
 
-//------------------------------------------------------------------------------
+/************************************************************************************/
 // number of blocks in the contiguous file
-const uint16_t BLOCK_COUNT = 10;
-//------------------------------------------------------------------------------
+#define BLOCK_COUNT 100
+/************************************************************************************/
 #define BUF_SIZE  512
 #define BUF_PTR_MASK  (BUF_SIZE-1)
-static byte buf1[BUF_SIZE];  // ring buffer 1
-static byte buf2[BUF_SIZE];  // ring buffer 2
+static byte buf1[BUF_SIZE];  // buffer 1
+static byte buf2[BUF_SIZE];  // buffer 2
 static volatile int buf1_rdPtr;
 static volatile int buf1_wrPtr;
 static volatile int buf2_rdPtr;
@@ -51,15 +47,20 @@ static uint8_t * pCache;
 #define ADC_START (ADCSRA = _BV(ADEN) | _BV(ADSC) | _BV(ADATE) | _BV(ADIE) | 0x04) // enable, start conversion, auto trigger, prescaler 128
 #define ADC_STOP (ADCSRA = 0)
 //-----------------------------------------------
-#define ADC_MUX_CHANNELS 4  // must be power of 2
+#define ADC_MUX_CHANNELS 4  // must be power of 2, ADC_MUX_INIT must be changed accordingly
 #define ADC_MUX_CHANNELS_MASK (ADC_MUX_CHANNELS-1)
-#define ADC_MUX_INIT  (_BV(REFS0) | _BV(ADLAR))  // Vcc as ref, left adjust for 8 bit resolution, use ADC0
+// setup mux: internal 1.1V / 2.56V as ref, left adjust for 8 bit resolution, use ADC4..7
+#define ADC_MUX_INIT  ( _BV(REFS1) | _BV(REFS0) | _BV(ADLAR) | _BV(MUX2) )
 static volatile byte adc_mux;
+static volatile byte adc_0; // very first conversion result
 /************************************************************************************/
 static void adc_setup ()
 {
-  pinMode(RXDATA, INPUT);
-  pinMode(RELAIS_PIN, OUTPUT);
+  // set pin mode of ADC4...7 to input
+  pinMode(A4, INPUT);
+  pinMode(A5, INPUT);
+  pinMode(A6, INPUT);
+  pinMode(A7, INPUT);
 //    digitalWrite(RXDATA, 1); // pull-up
 #ifdef DEBUG
   pinMode(ADC_OUT_PIN,OUTPUT);
@@ -67,14 +68,15 @@ static void adc_setup ()
   pinMode(DBG_PIN,OUTPUT);
   digitalWrite(DBG_PIN,0);
 #endif
+  /**/
+  adc_mux = 3;
+  adc_0 = 0;
   // set ADC0
   ADC_STOP;
-  ADMUX = ADC_MUX_INIT;
+  ADMUX = ADC_MUX_INIT | adc_mux; // init mux to ADC7
   //ADCSRA = _BV(ADEN) | _BV(ADSC) | _BV(ADATE) | _BV(ADIE) | 0x04; // enable, start conversion, auto trigger, prescaler 128
   ADCSRB = 0;  // free running mode
   DIDR0 = ~(0xC0 | _BV(ADC0D)); // disable other ADC input buffers
-  //
-  adc_mux = 0;
 }
 
 /************************************************************************************/
@@ -91,33 +93,37 @@ static void buf_init (void)
 /************************************************************************************/
 ISR(ADC_vect)
 {
-  if ( buf_wrSel==BUF1 ) {
-    // check overflow
-    if ( buf1_rdPtr<BUF_SIZE ) {
-      buf_of++; // overflow
-      return;
-    }
-    buf1[buf1_wrPtr] = ADCH;
-    if ( (++buf1_wrPtr)>=BUF_SIZE ) {
-      buf2_wrPtr = 0; // update pointer for next writing
-      buf_wrSel = BUF2; // switch to second buffer
-      digitalWrite(ADC_OUT_PIN,1);
-    }
+  if ( adc_0==0 ) {
+    adc_0 = 1; // don't store very first conversion result
   } else {
-    // check overflow
-    if ( buf2_rdPtr<BUF_SIZE ) {
-      buf_of++; // overflow
-      return;
-    }
-    buf2[buf2_wrPtr] = ADCH;
-    if ( (++buf2_wrPtr)>=BUF_SIZE ) {
-      buf1_wrPtr = 0; // update pointer for next writing
-      buf_wrSel = BUF1; // switch to first buffer
-      digitalWrite(ADC_OUT_PIN,0);
+    if ( buf_wrSel==BUF1 ) {
+      // check overflow
+      if ( buf1_rdPtr<BUF_SIZE ) {
+        buf_of++; // overflow
+        return;
+      }
+      buf1[buf1_wrPtr] = ADCH;
+      if ( (++buf1_wrPtr)>=BUF_SIZE ) {
+        buf2_wrPtr = 0; // update pointer for next writing
+        buf_wrSel = BUF2; // switch to second buffer
+        //digitalWrite(ADC_OUT_PIN,1);
+      }
+    } else {
+      // check overflow
+      if ( buf2_rdPtr<BUF_SIZE ) {
+        buf_of++; // overflow
+        return;
+      }
+      buf2[buf2_wrPtr] = ADCH;
+      if ( (++buf2_wrPtr)>=BUF_SIZE ) {
+        buf1_wrPtr = 0; // update pointer for next writing
+        buf_wrSel = BUF1; // switch to first buffer
+        //digitalWrite(ADC_OUT_PIN,0);
+      }
     }
   }
   // update ADC_MUX
-  ADMUX = ADC_MUX_INIT | ( adc_mux = (adc_mux+1) & ADC_MUX_CHANNELS_MASK);
+  ADMUX = ADC_MUX_INIT | ( adc_mux = (adc_mux-1) & ADC_MUX_CHANNELS_MASK);
 }
 /************************************************************************************/
 void sd_init()
@@ -162,37 +168,34 @@ void sd_write()
   uint32_t maxWriteTime = 0;
 
   uint32_t t = millis();
-  // write data to SD card
+  // acquire data to buffers
   for (uint16_t b = 0; b < BLOCK_COUNT; b++)
   {
     /**/
     if ( buf_wrSel==BUF1 ) {
-      // wait till buf 1 is full
+      //  ADC writes BUF1, wait till BUF1 is full
       while ( buf1_wrPtr<BUF_SIZE ) {};
-      // ADC writes buf1, so now is time for BUF2 to be read
+      // ADC finished writing BUF1, so now is time to read BUF1
       buf_rdSel = BUF1;
       buf1_rdPtr = 0;
       memcpy(pCache,buf1,512);
       buf1_rdPtr = BUF_SIZE;
     } else {
-      // ADC writes buf2, wait till buf 2 is full
+      // ADC writes BUF2, wait till BUF2 is full
       while ( buf2_wrPtr<BUF_SIZE ) {};
+      // ADC finished writing BUF2, so now is time to read BUF2
       buf_rdSel = BUF2;
       buf2_rdPtr = 0;
       memcpy(pCache,buf2,512);
       buf2_rdPtr = BUF_SIZE;
     }
 /**/
-    // put block number at start of first line in block
-    pCache[0] = (byte)b;
-    
-    // write a 512 byte block
+    // write a 512 byte cache block to SD card
     uint32_t tw = micros();
         digitalWrite(DBG_PIN,1);
       if (!sd.card()->writeData(pCache)) error("writeData failed");
         digitalWrite(DBG_PIN,0);
     tw = micros() - tw;
-    //ADC_START;
     // update max write time
     if (tw > maxWriteTime)  maxWriteTime = tw;
     //Serial.println(b);
@@ -206,7 +209,7 @@ void sd_write()
    // close file for next pass of loop
   file.close();
  
-  Serial.println(F("Done."));
+  Serial.println(F("Acquisition done."));
   Serial.print(F("Elapsed time [millis]: ")); Serial.println(t);
   Serial.print(F("Max write time [micros]: ")); Serial.println( maxWriteTime );
   Serial.print(F("Overruns: ")); Serial.println( buf_of );
